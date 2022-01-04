f142fefbc718e60f70464cf9e4d9439ab907062a
Glitched Polygons <glitchedpolygons@protonmail.com>
2022-01-04 19:22:47 +0100

    [WIP] Implemented show/hide password field button, prepared various UI elements and list widgets.

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -3,10 +3,11 @@
 
 #include "constants.h"
 #include <QSettings>
+#include <QFile>
+#include <QFileDialog>
+#include <QMessageBox>
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 
@@ -14,7 +15,6 @@ MainWindow::MainWindow(QWidget *parent)
     QSettings settings;
 
     const QString pfxFile = settings.value(Constants::Settings::pfxFile, QVariant("")).toString();
-
 }
 
 MainWindow::~MainWindow()
@@ -22,3 +22,98 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::on_browsePfxFilePushButton_clicked()
+{
+    const QString fileName = QFileDialog::getOpenFileName(this, "Select .pfx/.p12 file for signing", "", "PFX/P12 Signing File (*.p12 *.pfx)");
+
+    if (fileName.isEmpty())
+    {
+        return;
+    }
+
+    QFile file(fileName);
+
+    if (!file.exists()) [[unlikely]]
+    {
+        QMessageBox msgBox;
+        msgBox.setText("File does not exist or access to it failed.");
+        msgBox.exec();
+        return;
+    }
+
+    ui->pfxFileLineEdit->setText(fileName);
+}
+
+void MainWindow::on_showPfxFilePasswordPushButton_pressed()
+{
+    ui->pfxFilePasswordLineEdit->setEchoMode(QLineEdit::EchoMode::Normal);
+    ui->showPfxFilePasswordPushButton->setText("Hide");
+}
+
+void MainWindow::on_showPfxFilePasswordPushButton_released()
+{
+    ui->pfxFilePasswordLineEdit->setEchoMode(QLineEdit::EchoMode::Password);
+    ui->showPfxFilePasswordPushButton->setText("Show");
+}
+
+void MainWindow::on_signPushButton_clicked()
+{
+    if (busy)
+    {
+        return;
+    }
+
+    busy = true;
+
+    const QString fileName = ui->pfxFileLineEdit->text();
+
+    if (fileName.isEmpty())
+    {
+        ui->signOutputTextEdit->setText("No .pfx/.p12 file selected... Please provide a valid file to use for signing!\n\nUse the \"Browse\" button above and select the code signing certificate to use!");
+        busy = false;
+        return;
+    }
+
+    QFile file(fileName);
+
+    if (!file.exists()) [[unlikely]]
+    {
+        ui->signOutputTextEdit->setText("The specified .pfx/.p12 file does not exist or access to it failed.");
+        busy = false;
+        return;
+    }
+
+    // TODO: sign all files here
+    busy = false;
+}
+
+void MainWindow::on_clearFilesToSignListPushButton_clicked()
+{
+    ui->filesToSignListWidget->clear();
+}
+
+void MainWindow::on_verifyFilesPushButton_clicked()
+{
+    if (busy)
+    {
+        return;
+    }
+
+    busy = true;
+
+    if (ui->filesToVerifyListWidget->count() == 0)
+    {
+        ui->filesVerificationOutputTextEdit->setText("No files to verify.\n\nPlease drag & drop one or more binaries into the list above to proceed!");
+        busy = false;
+        return;
+    }
+
+    // TODO: verify list of files here
+
+    busy = false;
+}
+
+void MainWindow::on_clearFilesToVerifyListPushButton_clicked()
+{
+    ui->filesToVerifyListWidget->clear();
+}
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -4,7 +4,9 @@
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+namespace Ui {
+class MainWindow;
+}
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
@@ -12,10 +14,26 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    MainWindow(QWidget* parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_browsePfxFilePushButton_clicked();
+
+    void on_showPfxFilePasswordPushButton_pressed();
+
+    void on_showPfxFilePasswordPushButton_released();
+
+    void on_signPushButton_clicked();
+
+    void on_clearFilesToSignListPushButton_clicked();
+
+    void on_verifyFilesPushButton_clicked();
+
+    void on_clearFilesToVerifyListPushButton_clicked();
+
 private:
-    Ui::MainWindow *ui;
+    Ui::MainWindow* ui;
+    bool busy = false;
 };
 #endif // MAINWINDOW_H
